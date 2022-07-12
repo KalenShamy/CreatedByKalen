02cdaef163bc5dc8e342f5c855eb19b16e59414b
KalenShamy <kalen.shamy@gmail.com>
2022-07-12 19:15:02 -0400

    further image square compatibility

--- script.js
+++ script.js
@@ -92,7 +92,7 @@ function setUpSlideshowHover() {
   slide.info = slide.getBoundingClientRect();
   let image = document.getElementById("projects").children[1].children[1].children[0];
   image.imageSize = function() {
-    let size = slide.info.width < slide.info.height && slide.info.width || slide.info.height;
+    let size = image.width < slide.height && slide.width || slide.height;
     image.width = size;
     image.height = size;
   }
